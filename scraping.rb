116483e6bf24e191e1f3a349a36c0ffef8f488e8
{asato2000} <asato@kudouasatoshinoMacBook-Pro.local>
2021-08-16 21:28:07 +0900

    全体取得

--- scraping.rb
+++ scraping.rb
@@ -13,17 +13,21 @@ end
 html = File.open('pitnews.html', 'r') {|f| f.read }
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 
-section = doc.xpath('/html/body/main/section[2]').first
 
-contents = {category: nil, news: []}
-contents[:category] = section.xpath('./h6').first.text
-section.xpath('./div/div').each do |node|
-  title = node.xpath('./p/strong/a').first.text
-  url = node.xpath('./p/strong/a').first['href']
+pitnews = []
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
 
-  # === ここから追加および編集
-  news = {title: title, url: url}
-  contents[:news] << news
-  # === ここまで
+  next if index.zero?
+  contents = {category: nil, news: []}
+  contents[:category] = section.xpath('./h6').first.text
+
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+
+  pitnews << contents
 end
-pp contents
