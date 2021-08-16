02b1b1995d131ae0b25563695248067ef56d90bd
{asato2000} <asato@kudouasatoshinoMacBook-Pro.local>
2021-08-16 21:27:02 +0900

    ニュース一覧の取得

--- scraping.rb
+++ scraping.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require "nokogiri"
 
 def get_from(url)
   Net::HTTP.get(URI(url))
@@ -8,4 +8,22 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
-write_file('pitnews.html', get_from('https://masayuki14.github.io/pit-news/'))
+
+
+html = File.open('pitnews.html', 'r') {|f| f.read }
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
+section = doc.xpath('/html/body/main/section[2]').first
+
+contents = {category: nil, news: []}
+contents[:category] = section.xpath('./h6').first.text
+section.xpath('./div/div').each do |node|
+  title = node.xpath('./p/strong/a').first.text
+  url = node.xpath('./p/strong/a').first['href']
+
+  # === ここから追加および編集
+  news = {title: title, url: url}
+  contents[:news] << news
+  # === ここまで
+end
+pp contents
